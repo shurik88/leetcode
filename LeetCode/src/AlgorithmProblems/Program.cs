131e49eab5788c58a0064a250002fef746650d7c
Александр Будыльский <budylsky@adeptik.com>
2017-09-17 14:16:20 +0300

    init project

---
+++ LeetCode/src/AlgorithmProblems/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var problem = new TwoSum_1();
+            var res = problem.TwoSum(new int[] {3, 2, 4}, 6);
+        }
+    }
+}
---
+++ LeetCode/src/AlgorithmProblems/TwoSum_1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+    public class TwoSum_1
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            QuickSort(nums, 0, nums.Length - 1);
+            int rightIndex = nums.Length - 1;
+            int leftIndex = 0;
+            while (true)
+            {
+                var sum = nums[leftIndex] + nums[rightIndex];
+                if (sum == target)
+                    return new int[2] { leftIndex, rightIndex };
+                else if (sum > target)
+                    rightIndex--;
+                else
+                    leftIndex++;
+            }
+        }
+
+        static readonly Random Rand = new Random();
+
+        private static void QuickSort(int[] arr, int left, int right)
+        {
+            if (left == right)
+                return;
+            var x = arr[Rand.Next(left, right)];
+            var i = left;
+            var j = right;
+            while (i <= j)
+            {
+                while (arr[i] < x)
+                    i++;
+                while (arr[j] > x)
+                    j--;
+                if (i <= j)
+                {
+                    Swap(arr, i, j);
+                    i++;
+                    j--;
+                }
+            }
+
+            if (i < right)
+                QuickSort(arr, i, right);
+            if (left < j)
+                QuickSort(arr, left, j);
+        }
+
+        private static void Swap(int[] arr, int i, int j)
+        {
+            var temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
